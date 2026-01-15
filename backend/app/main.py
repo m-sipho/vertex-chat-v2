edf0e2582a66339c26cb6ca4844c491b1a900118
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-15 13:32:19 +0200

    Implement the exception handlers

---
+++ backend/app/main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from .services.room_manager import Room_Manager
+from .services.exceptions import (
+    NoAvailableRoomError,
+    UserAlreadyInRoomError,
+    UserAlreadyInAwaitingError,
+    UserNotAuthorizedError,
+    UserNotFoundError,
+    RoomNotFoundError
+)
+
+
+app = FastAPI(title="Vertex Backend")
+
+@app.exception_handler(NoAvailableRoomError)
+async def server_full_handler(request: Request, exc: NoAvailableRoomError):
+    return JSONResponse(
+        status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+        content={"status": "error", "message": str(exc)}
+    )
+
+@app.exception_handler(UserAlreadyInRoomError)
+async def user_in_room_conflict_handler(request: Request, exc: UserAlreadyInRoomError):
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={"status": "error", "message": str(exc)}
+    )
+
+@app.exception_handler(UserAlreadyInAwaitingError)
+async def user_in_awaiting_handler(request: Request, exc: UserAlreadyInAwaitingError):
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={"status": "error", "message": str(exc)}
+    )
+
+@app.exception_handler(UserNotAuthorizedError)
+async def unauthorized_handler(request: Request, exc: UserNotAuthorizedError):
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"status": "Unathorized", "message": str(exc)}
+        )
+
+@app.exception_handler(UserNotFoundError)
+async def user_not_found_handler(request: Request, exc: UserNotFoundError):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"status": "user not found", "message": str(exc)}
+    )
+
+@app.exception_handler(RoomNotFoundError)
+async def room_not_found_handler(request: Request, exc: RoomNotFoundError):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"status": "room not found", "message": str(exc)}
+    )
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello world"}
