fa102fae27684211a2bcc6857f9a63e14d6b4d91
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-02-02 19:22:41 +0200

    Initialize react, tailwind, and implement login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path={""} element={<LoginPage />} />
+    </Routes>
+  )
+}
+
+export default App
