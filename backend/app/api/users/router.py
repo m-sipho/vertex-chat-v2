607eea5f9290e067b56a65e9bf8cb6359b678e6b
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-02-05 00:21:34 +0200

    Set an endpoint to change display name

--- backend/app/api/users/router.py
+++ backend/app/api/users/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db
-from .schemas import CreateUser
-from .services import create_user
+from .schemas import CreateUser, UserID
+from .services import create_user, change_display_name
+from app.api.auth.dependencies import get_current_user
+from typing import Annotated
 
 
 router = APIRouter()
@@ -10,3 +12,7 @@ router = APIRouter()
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register(user_data: CreateUser, db: AsyncSession = Depends(get_db)):
     return await create_user(user_data=user_data, db=db)
+
+@router.post("/set-display-name", status_code=status.HTTP_200_OK)
+async def set_display_name(display_name: str, current_user: Annotated[UserID, Depends(get_current_user)],  db: AsyncSession = Depends(get_db)):
+    return await change_display_name(display_name, current_user, db)
--- backend/app/api/users/schemas.py
+++ backend/app/api/users/schemas.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
+import uuid
 
 class CreateUser(BaseModel):
     username: str
     password: str
     display_name: Optional[str] = None
+
+class UserID(BaseModel):
+    id: uuid.UUID
