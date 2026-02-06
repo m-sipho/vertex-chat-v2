0e1ea3180646b0712bd45a1824320db15486d8ff
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-02-06 18:29:05 +0200

    Implement createRoom API function for room creation

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -56,3 +56,9 @@ export async function registerUser(username, password, display_name = undefined)
         })
     })
 }
+
+export async function createRoom(title) {
+    return apiClient(`/create-room?title=${encodeURIComponent(title)}`, () => (sessionStorage.getItem("token")), {
+        method: "POST",
+    })
+}
